e14c7df243d138c1535882f645fc42da97ed7802
LizaGrebenshchikova <grebenshchikova.liza@yahoo.com>
2019-02-26 22:35:41 +0300

    Added test server

---
+++ testServer/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const server = express();
+server.use(express.static(__dirname + '/../build'));
+
+const port = 3000;
+server.listen(port, () => {
+    console.log('server is listening on port ' + port)
+});
