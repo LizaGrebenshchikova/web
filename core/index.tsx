f9a18eead0b993e57e385dec60e6181c20df3650
LizaGrebenshchikova <grebenshchikova.liza@yahoo.com>
2019-02-26 22:23:21 +0300

    Main index added

---
+++ core/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>
+        Cute app there
+    </div>,
+    document.getElementById('app-container')
+);
