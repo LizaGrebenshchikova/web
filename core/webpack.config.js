2240cf91036af65e9b095d4952ebe4326317a9c4
LizaGrebenshchikova <grebenshchikova.liza@yahoo.com>
2019-04-07 19:42:25 +0300

    Added less imports

--- core/webpack.config.js
+++ core/webpack.config.js
@@ -1,26 +1,34 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './index.tsx',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.less$/,
-        loader: 'less-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../build')
-  }
+	entry: './index.tsx',
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{ 
+                test: /\.less$/,
+                use: [ 
+                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 
+                    'css-loader', 
+                    'less-loader'
+                ],
+            }
+		]
+	},
+    plugins: isProduction ? [new MiniCssExtractPlugin()] : [],
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, '../build')
+	}
 };
