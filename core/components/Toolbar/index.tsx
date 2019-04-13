d16192222fc259079474b315291411c5955f6e7b
LizaGrebenshchikova <grebenshchikova.liza@yahoo.com>
2019-04-13 11:28:15 +0300

    Added canvas + style fixes

--- core/components/Toolbar/index.tsx
+++ core/components/Toolbar/index.tsx
@@ -5,8 +5,14 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 
+interface ToolbarCallbacks {
+    onClickSave: () => void;
+}
+
 interface ToolbarProps {
     prefixCls?: string;
+
+    callbacks: ToolbarCallbacks;
 }
 
 export default class Toolbar extends React.Component<ToolbarProps> {
@@ -34,7 +40,18 @@ export default class Toolbar extends React.Component<ToolbarProps> {
                     <Button variant='outline-primary'>⭯</Button>
                     <Button variant='outline-primary'>⭮</Button>
                 </ButtonGroup>
+                <Button 
+                    className={`${prefixCls}__btn-save`} 
+                    variant='outline-warning' 
+                    onClick={this.handleClickSave}
+                >
+                    Save collage
+                </Button>
             </ButtonToolbar>
         );
+    }
+
+    private handleClickSave() {
+        this.props.callbacks.onClickSave();
     }
 }
