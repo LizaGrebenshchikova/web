883f2af6ca9588bd4e68b11565eeb9567fcee8c6
LizaGrebenshchikova <grebenshchikova.liza@yahoo.com>
2019-05-06 17:02:12 +0300

    Photo refactoring

--- core/components/Photo/index.tsx
+++ core/components/Photo/index.tsx
@@ -6,12 +6,19 @@ import Image from 'react-bootstrap/Image';
 
 interface PhotoProps {
     url: string;
+    prefixCls?: string;
 }
 
 export default class Photo extends React.Component<PhotoProps> {
+    static defaultProps = {
+        prefixCls: 'photo'
+    }
+
     render() {
+        const { prefixCls } = this.props;
+
         return (
-            <div className='photo'>
+            <div className={prefixCls}>
                 <Image src={this.props.url}/>
             </div>
         );
